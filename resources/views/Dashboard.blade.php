c54c4940a7a8f48f9ec6cda10a76c103c475827d
Lukarrio1 <40766583+Lukarrio1@users.noreply.github.com>
2019-01-23 22:11:36 -0500

    time zone fix

--- resources/views/Dashboard.blade.php
+++ resources/views/Dashboard.blade.php
@@ -4,17 +4,14 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
+            <div class="card border-success">
+                <div class="card-header h3 text-center bg-white">Content Management System</div>
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                    <div class="">
+                        <a href="" class="btn btn-default">
+                            <span class="h4">Fenntech CMS </span>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
