c99aa58648400c19de96bdfa3995e06c2e660c47
Lukarrio1 <40766583+Lukarrio1@users.noreply.github.com>
2019-01-27 14:34:17 -0500

    the basic layout

--- resources/views/Landing.blade.php
+++ resources/views/Landing.blade.php
@@ -5,5 +5,37 @@
 @endsection
 
 @section('content')
-<a href="{{route('Fenntech')}}" class="btn btn-success">Fenntech link</a>
+@section('content')
+<div class="row">
+    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 pb-2">
+    <div class="card shadow rounded">
+        <div class="card-body text-center">
+                <a href="{{route('Fenntech')}}" class="btn btn-lg btn-success">Fenntech Home</a>
+        </div>
+    </div>
+    </div>
+    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 pb-2">
+            <div class="card shadow rounded">
+                    <div class="card-body text-center">
+                        <a href="#" class="btn btn-primary btn-lg text-white">Fenntech Bicycle</a>
+                    </div>
+                </div>
+    </div>
+    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <div class="card shadow rounded">
+                    <div class="card-body text-center">
+                        <a href="#" class="btn btn-primary btn-lg text-white">Fenntech Wedding</a>
+                    </div>
+                </div>
+    </div>
+    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            <div class="card shadow rounded">
+                    <div class="card-body text-center">
+                        <a href="#" class="btn btn-success btn-lg text-white">Fenntech Corporate</a>
+                    </div>
+                </div>
+    </div>
+</div>
+@endsection
+
 @endsection
