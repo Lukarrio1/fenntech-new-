e109fe1e263fdba8c0a259ea8bab99744c112256
Lukarrio1 <40766583+Lukarrio1@users.noreply.github.com>
2019-01-28 09:18:48 -0500

    404 page added

--- resources/views/Landing.blade.php
+++ resources/views/Landing.blade.php
@@ -16,33 +16,21 @@
     <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 pb-2">
             <div class="card shadow rounded">
                     <div class="card-body text-center">
-<<<<<<< HEAD
-                    <a href="{{route('unavailable')}}" class="btn btn-primary btn-lg text-white">Fenntech Bicycle</a>
-=======
-                        <a href="#" class="btn btn-primary btn-lg text-white">FennTech Corporate</a>
->>>>>>> 47ff7140aa192ab4d902a0a8bd3057aafd6de7bb
+                    <a href="{{route('unavailable')}}" class="btn btn-primary btn-lg text-white">FennTech Corporate</a>
                     </div>
                 </div>
     </div>
     <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
             <div class="card shadow rounded">
                     <div class="card-body text-center">
-<<<<<<< HEAD
-                        <a href="{{route('unavailable')}}" class="btn btn-primary btn-lg text-white">Fenntech Wedding</a>
-=======
-                        <a href="#" class="btn btn-primary btn-lg text-white">Software Division</a>
->>>>>>> 47ff7140aa192ab4d902a0a8bd3057aafd6de7bb
+                        <a href="{{route('unavailable')}}" class="btn btn-primary btn-lg text-white">Software Division</a>
                     </div>
                 </div>
     </div>
     <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
             <div class="card shadow rounded">
                     <div class="card-body text-center">
-<<<<<<< HEAD
-                        <a href="{{route('unavailable')}}" class="btn btn-success btn-lg text-white">Fenntech Corporate</a>
-=======
-                        <a href="#" class="btn btn-success btn-lg text-white">IT Services Div</a>
->>>>>>> 47ff7140aa192ab4d902a0a8bd3057aafd6de7bb
+                        <a href="{{route('unavailable')}}" class="btn btn-success btn-lg text-white">IT Services Div</a>
                     </div>
                 </div>
     </div>
