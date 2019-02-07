64aeb1a5004f55b1534c06db379217d9cfbe9f53
Lukarrio1 <40766583+Lukarrio1@users.noreply.github.com>
2019-02-07 15:23:37 -0500

    small fix

--- resources/views/Landing.blade.php
+++ resources/views/Landing.blade.php
@@ -5,36 +5,28 @@
 @endsection
 
 @section('content')
-<div class= "fenncontainer">
-<div id="carouselExampleIndicators" class="carousel slide carousel-fade" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>
-
+<div class="fenncontainer">
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" style="height: 100%;" src="/storage/damian-patkowski-449653-unsplash.jpg" alt="First slide">
+      <img src=""  class="d-block w-100" alt="...">
       <div class="carousel-caption d-none d-md-block">
-    <h5>FennTech</h5>
-    <p>sksksksks</p>
-  </div>
-    </div>
-</div>
+        <h5>FennTech</h5>
+        <p>Hello there is the the fenntech site</p>
+      </div>
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src=".../800x400?auto=yes&bg=666&fg=444&text=Second slide" alt="Second slide">
+      <img src="..." class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src=".../800x400?auto=yes&bg=555&fg=333&text=Third slide" alt="Third slide">
+      <img src="..." class="d-block w-100" alt="...">
     </div>
   </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
   </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
   </a>
