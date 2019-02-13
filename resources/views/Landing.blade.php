db5c43cedf72c43c529ccf0cdb3a3e9f14a3fdc8
Lukarrio1 <40766583+Lukarrio1@users.noreply.github.com>
2019-02-13 13:31:40 -0500

    cleaming up some files

--- resources/views/Landing.blade.php
+++ resources/views/Landing.blade.php
@@ -9,7 +9,7 @@
 <div class="col-md-8 offset-md-2 pt-5">
 <div class="card shadow rounded">
   <div class="card-body">
-      {Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque nihil iusto praesentium amet cum,
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque nihil iusto praesentium amet cum,
       nisi consequatur neque dolor. Voluptatum,
   </div>
   </div>
