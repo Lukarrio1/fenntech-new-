fe3a652aae9d048fc80527526992ac8084a5fdee
theboostkid <russelltimothy2@gmail.com>
2019-02-15 12:19:41 -0500

    chane to the networking route

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::resource('/Networing','NetworkingController');
+Route::resource('/Networking','NetworkingController');
 
 Route::get('/','PagesController@landingpage')->name('Landing');
 
