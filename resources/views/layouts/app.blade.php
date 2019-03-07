452e1e705742626aff00dc8c6f5e8d8508a5d440
theboostkid <russelltimothy2@gmail.com>
2019-03-07 16:47:53 -0500

    homepage

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,20 +15,80 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <!-- CSS
+  ================================================== -->
+  <!-- RS5.0 Main Stylesheet -->
+  <link rel="stylesheet" type="text/css" href="plugins/revo-slider/css/settings.css">
+  <!-- RS5.0 Layers and Navigation Styles -->
+  <link rel="stylesheet" type="text/css" href="plugins/revo-slider/css/layers.css">
+  <link rel="stylesheet" type="text/css" href="plugins/revo-slider/css/navigation.css">
+  <!-- REVOLUTION STYLE SHEETS -->
+  <link rel="stylesheet" type="text/css" href="plugins/revo-slider/fonts/pe-icon-7-stroke/css/pe-icon-7-stroke.css">
+  <link rel="stylesheet" type="text/css" href="plugins/revo-slider/fonts/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="plugins/revo-slider/css/settings.css">
+  <link rel="stylesheet" type="text/css" href="plugins/revo-slider/css/layers.css">
+  <link rel="stylesheet" type="text/css" href="plugins/revo-slider/css/navigation.css">
+  <!-- Themefisher Icon font -->
+  <link rel="stylesheet" href="plugins/themefisher-font/style.css">
+  <!-- Themefisher Icon font -->
+  <link rel="stylesheet" href="{{ asset('plugins/revo-slider/css/navigation.css') }}">
+  <!-- bootstrap.min css -->
+  <link rel="stylesheet" href="{{ asset('plugins/bootstrap/css/bootstrap.min.css') }}">
+  <!-- Lightbox.min css -->
+  <link rel="stylesheet" href="{{ asset('plugins/lightbox2/dist/css/lightbox.min.css') }}">
+  <!-- Slick Carousel -->
+  <link rel="stylesheet" href="{{ asset('plugins/slick-carousel/slick/slick.css') }}">
+  <link rel="stylesheet" href="{{ asset('plugins/slick-carousel/slick/slick-theme.css') }}">
+  <!-- Main Stylesheet -->
+  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+
+  <!-- Colors -->
+  <link rel="stylesheet" type="text/css" href="{{ asset('css/colors/green.css') }}" title="green">
+  <link rel="stylesheet" type="text/css" href="{{ asset('css/colors/red.css') }}" title="light-red">
+  <link rel="stylesheet" type="text/css" href="{{ asset('css/colors/blue.css') }}" title="blue">
+  <link rel="stylesheet" type="text/css" href="{{ asset('css/colors/light-blue.css') }}" title="light-blue">
+  <link rel="stylesheet" type="text/css" href="{{ asset('css/colors/yellow.css') }}" title="yellow">
+  <link rel="stylesheet" type="text/css" href="{{ asset('css/colors/light-green.css') }}" title="light-green">
+
 </head>
 <body>
     <div id="app">
-@include('inc.nav')
+{{--  @include('inc.nav')
         <main class="py-4">
           <div class="row">
             @yield('content')
         </div>
         </main>
-    </div>
+    </div>  --}}
  <script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
+
+
+    <script src="plugins/jquery/dist/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
+    <script  src="{{ asset('plugins/google-map/gmap.js') }}"></script>
+
+    <!-- Parallax -->
+    <script src="plugins/parallax/jquery.parallax-1.1.3.js"></script>
+    <!-- lightbox -->
+    <script src="plugins/lightbox2/dist/js/lightbox.min.js"></script>
+    <!-- Owl Carousel -->
+    <script src="plugins/slick-carousel/slick/slick.min.js"></script>
+    <!-- Portfolio Filtering -->
+    <script src="plugins/mixitup/dist/mixitup.min.js"></script>
+    <!-- Smooth Scroll js -->
+    <script src="plugins/smooth-scroll/dist/js/smooth-scroll.min.js"></script>
+
+    <!-- Custom js -->
+    <script src="js/script.js"></script>
+
 </body>
 </html>
