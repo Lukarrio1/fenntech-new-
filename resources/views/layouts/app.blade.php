541605fe9ca66401f4ab3a944bfdd9196820a288
lemarhenry <lemarhenry97@gmail.com>
2019-03-07 17:52:59 -0500

    'gfuy'

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,6 +27,7 @@
   <link rel="stylesheet" type="text/css" href="plugins/revo-slider/css/layers.css">
   <link rel="stylesheet" type="text/css" href="plugins/revo-slider/css/navigation.css">
   <!-- REVOLUTION STYLE SHEETS -->
+  
   <link rel="stylesheet" type="text/css" href="plugins/revo-slider/fonts/pe-icon-7-stroke/css/pe-icon-7-stroke.css">
   <link rel="stylesheet" type="text/css" href="plugins/revo-slider/fonts/font-awesome/css/font-awesome.min.css">
   <link rel="stylesheet" type="text/css" href="plugins/revo-slider/css/settings.css">
@@ -47,6 +48,7 @@
   <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 
 
+
   <!-- Colors -->
   <link rel="stylesheet" type="text/css" href="{{ asset('css/colors/green.css') }}" title="green">
   <link rel="stylesheet" type="text/css" href="{{ asset('css/colors/red.css') }}" title="light-red">
@@ -58,13 +60,13 @@
 </head>
 <body>
     <div id="app">
-{{--  @include('inc.nav')
+ {{--  @include('inc.nav')  --}}
         <main class="py-4">
           <div class="row">
             @yield('content')
         </div>
         </main>
-    </div>  --}}
+    </div> 
  <script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
 
 
@@ -72,23 +74,23 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    {{-- <script src="{{asset('free/js/script.js')}}"> --}}
 
-
-    <script  src="{{ asset('plugins/google-map/gmap.js') }}"></script>
+    <script src="{{ asset('plugins/google-map/gmap.js') }}"></script>
 
     <!-- Parallax -->
-    <script src="plugins/parallax/jquery.parallax-1.1.3.js"></script>
+    <script src="{{asset('plugins/parallax/jquery.parallax-1.1.3.js')}}"></script>
     <!-- lightbox -->
-    <script src="plugins/lightbox2/dist/js/lightbox.min.js"></script>
+    <script src="{{asset('plugins/lightbox2/dist/js/lightbox.min.js')}}"></script>
     <!-- Owl Carousel -->
-    <script src="plugins/slick-carousel/slick/slick.min.js"></script>
+    <script src="{{asset('plugins/slick-carousel/slick/slick.min.js')}}"></script>
     <!-- Portfolio Filtering -->
-    <script src="plugins/mixitup/dist/mixitup.min.js"></script>
+    <script src="{{asset('plugins/mixitup/dist/mixitup.min.js')}}"></script>
     <!-- Smooth Scroll js -->
-    <script src="plugins/smooth-scroll/dist/js/smooth-scroll.min.js"></script>
+    <script src="{{asset('plugins/smooth-scroll/dist/js/smooth-scroll.min.js')}}"></script>
 
     <!-- Custom js -->
-    <script src="js/script.js"></script>
+    <script src="{{asset('js/script.js')}}"></script>
 
 </body>
 </html>
